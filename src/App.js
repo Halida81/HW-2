7356e5316d06586a4226c9232dbf3123e5a8bb11
holinur <halidas81@gmail.com>
2022-01-14 03:24:53 +0600

    hw

--- src/App.js
+++ src/App.js
@@ -2,14 +2,32 @@
 import './App.css';
 import Comment from './components/Comment'
 import Card from './ui/Card'
-import Home from './components/imgs/пташка.jpg'
+import Bird from './components/imgs/пташка.jpg'
+import Panda from './components/imgs/panda.jpg'
+import Fish from './components/imgs/рыбка.png'
+
 
 const comment = {
-  date: new Date(),
-  text: 'Learn React is awesome!',
+  date: new Date(2022, 11, '03'),
+  text: 'Learn Java Script is awesome!',
   author: {
     name: 'Пташка',
-    avatarUrl: Home,
+    avatarUrl: Bird,
+  },
+};
+const comment2 = {
+  date: new Date(),
+  text: 'Learn React is awesome! ',
+  author: {
+    name: 'Рыбка',
+    avatarUrl: Fish,
+  },
+};const comment3 = {
+  date: new Date(2022, '07', 19),
+  text: 'Learn Redux is awesome!',
+  author: {
+    name: 'Панда',
+    avatarUrl: Panda,
   },
 };
 
@@ -18,9 +36,26 @@ function App() {
 
   
   return (
-    <Card>
+    // <Card>
+    //   <Comment author={comment.author} date={comment.date} text={comment.text}/>
+    //   <Comment author={comment2.author} date={comment2.date} text={comment2.text}/>
+    //   <Comment author={comment3.author} date={comment3.date} text={comment3.text}/>
+    // </Card>
+    <>
+     <Card>
       <Comment author={comment.author} date={comment.date} text={comment.text}/>
     </Card>
+
+     <Card>
+       <Comment author={comment2.author} date={comment2.date} text={comment2.text}/>
+     </Card>
+
+      <Card>
+      <Comment author={comment3.author} date={comment3.date} text={comment3.text}/>
+      </Card>
+    
+    </>
+   
   );
 }
 
